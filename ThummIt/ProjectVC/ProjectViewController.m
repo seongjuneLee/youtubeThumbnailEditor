99549fec38e1031334f59baac380cc1ded56dd49
seongjuneLee <saintjune3950@gmail.com>
2021-01-17 03:31:55 +0900

    no message

--- ThummIt/ProjectVC/ProjectViewController.m
+++ ThummIt/ProjectVC/ProjectViewController.m
@@ -13,7 +13,7 @@
 
 @implementation ProjectViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad { 
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self connectProjectTableController];
