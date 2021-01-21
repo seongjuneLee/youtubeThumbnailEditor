49aaf23e561c231aee8d756141e6082ca7ff00c5
seongjuneLee <saintjune3950@gmail.com>
2021-01-21 20:28:09 +0900

    .

--- ThummIt/HomeVC/HomeViewController.m
+++ ThummIt/HomeVC/HomeViewController.m
@@ -24,23 +24,7 @@
     
     [self.tableView registerNib:[UINib nibWithNibName:@"HomeTableViewCell" bundle:NSBundle.mainBundle] forCellReuseIdentifier:@"HomeTableViewCell"];
     [self connectHomeTableController];
-    GuideLine *guideLine = [[GuideLine alloc] init];
-    guideLine.frameWidth = self.view.frameWidth;
-    guideLine.center = self.view.center;
-    guideLine.frameY = 100;
-    [self.view addSubview:guideLine];
-    NSLog(@"guideLine frame %@",NSStringFromCGRect(guideLine.frame));
     
-    DashedGuideLine *dashed = [[DashedGuideLine alloc] initWithFrame:CGRectMake(0, 250, self.view.frameWidth, 2)];
-    dashed.frameWidth = self.view.frameWidth;
-    dashed.center = CGPointMake(self.view.center.x, self.view.centerY - 10);
-    dashed.frameY = 150;
-    [dashed makeViewDashed];
-    [self.view addSubview:dashed];
-
-
-    NSLog(@"dashed frame %@",NSStringFromCGRect(dashed.frame));
-
 }
 
 -(void)connectHomeTableController{
